995e90f8972453f917c18d70c7de30d93ff8589a
veoflixapp-netizen <veoflixapp@gmail.com>
2026-09-08 12:48:11 +0800

    Update ads_config.js

--- ads_config.js
+++ ads_config.js
@@ -1,7 +1,7 @@
 {
   "interstitial": {
     "enabled": true,
-    "unit_id": "ca-app-pub-9563352434607342/3994621254",
+    "unit_id": "ca-app-pub-9868073814793133/4059054066",
     "interval_episodes": 4
   }
 }
